c1d5da74c227a4d76f7d9dd06cb4fcfcd66aba17
noobiwankenoobi <ross.hazelett@gmail.com>
2017-05-07 17:46:39 -0400

    Added "update" button
    Added it to the ui.js file in the html insert into table

--- assets/scripts/moviePosts/ui.js
+++ assets/scripts/moviePosts/ui.js
@@ -14,7 +14,7 @@ const getMoviesSuccess = (data) => {
   store.moviePosts = data.movie_posts
   let tableHTML = ''
   store.moviePosts.forEach((post) => {
-    tableHTML += '<tr><td>' + post.title + '</td><td>' + post.director + '</td><td>' + post.comment + '</td> <td> <button class="btn btn-danger btn-sm" data-id="delete">Delete Movie</button></td></tr>'
+    tableHTML += '<tr><td>' + post.title + '</td><td>' + post.director + '</td><td>' + post.comment + '</td> <td><button class="btn btn-warning btn-sm" data-id="update">Update Movie</button><button class="btn btn-danger btn-sm" data-id="delete">Delete Movie</button></td></tr>'
   })
   $('#my-movies-table-body').html(tableHTML)
   $('#my-movies-table').show()
